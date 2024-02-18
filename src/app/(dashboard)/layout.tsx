3b64611d2c5ac69ae3740bca63b3be27372899ca
Sayandeep <sayandeep11@gmail.com>
2024-02-19 04:39:32 +0530

    stat card done

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import Logo from "@/components/Logo";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
-// import { UserButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
 import React, { ReactNode } from "react";
 
 function Layout({ children }: { children: ReactNode }) {
@@ -10,7 +10,7 @@ function Layout({ children }: { children: ReactNode }) {
         <Logo />
         <div className="flex gap-4 items-center">
           <ThemeSwitcher />
-          {/* <UserButton afterSignOutUrl="/sign-in" /> */}
+          <UserButton afterSignOutUrl="/sign-in" />
         </div>
       </nav>
       <main className="flex w-full flex-grow">{children}</main>
