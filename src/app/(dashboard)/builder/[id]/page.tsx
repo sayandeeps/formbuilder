4ba7f0f2a5d8d660958cd95b0d4ef1bec413351e
Sayandeep <sayandeep11@gmail.com>
2024-02-20 15:48:06 +0530

    Builder NavBar Done

--- src/app/(dashboard)/builder/[id]/page.tsx
+++ src/app/(dashboard)/builder/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { GetFormById } from "@/actions/form";
-// import FormBuilder from "@/components/FormBuilder";
+import FormBuilder from "@/components/FormBuilder";
 import React from "react";
 
 async function BuilderPage({
@@ -14,8 +14,7 @@ async function BuilderPage({
   if (!form) {
     throw new Error("form not found");
   }
-//   return <FormBuilder form={form} />;
-return <>hello</>
+  return <FormBuilder form={form} />;
 }
 
 export default BuilderPage;
