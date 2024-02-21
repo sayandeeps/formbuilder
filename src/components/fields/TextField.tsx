98979f38bf31846a16cbe330b7648c9d1679a998
Sayandeep <sayandeep11@gmail.com>
2024-02-21 17:50:00 +0530

    internal drag and drop working

--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -36,7 +36,7 @@ function DesignerComponent({elementInstance}:{elementInstance : FormElementInsta
   return (
     <div className="flex flex-col gap-2 w-full">
       <Label>
-        {label}
+        {label} {element.id}
         {required && "*"}
       </Label>
       <Input readOnly disabled placeholder={placeHolder} />
